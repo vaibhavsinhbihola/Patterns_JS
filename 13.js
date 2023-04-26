a0e09ac7804e63b0396199b92ef6a011a31745b1
vaibhavsinhbihola <vaibhavsinhbihola26@gmail.com>
2023-04-26 17:43:26 +0530

    26/04/23

---
+++ 13.js
@@ -0,0 +1,15 @@
+const n = 5;
+
+for (let i = 1; i <= n; i++) {
+    for (let space = 1; space <= n - i; space++) {
+        process.stdout.write(" ");
+    }
+    for (let j = 1; j <= (2 * i) - 1; j++) {
+        if (j === 1 || j === (2 * i) - 1 || i === n) {
+            process.stdout.write("*");
+        } else {
+            process.stdout.write(" ");
+        }
+    }
+    console.log();
+}
